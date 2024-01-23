685ae93aaf67d2dc254ab42f6bf669c2f4df1e10
Zubair <syedzubi270@gmail.com>
2024-01-22 19:54:25 -0500

    working model

--- FrontEnd/my-xkcd-app/src/App.tsx
+++ FrontEnd/my-xkcd-app/src/App.tsx
@@ -1,35 +1,72 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Comic } from "./types"; // adjust the path as necessary
-import { ComicDisplay } from "./components/ComicDisplay/ComicDisplay";
 
 const App = () => {
-  const [latestComic, setLatestComic] = useState<Comic | null>(null);
+  const [xkcdData, setXkcdData] = useState<any>(null);
+  const [latestComicNumber, setLatestComicNumber] = useState<any>(0);
 
   useEffect(() => {
-    fetch("/xkcd/latest")
-      .then((response) => response.json())
-      .then((data) => setLatestComic(data));
+    fetchLatestComic();
   }, []);
 
+  const fetchComic = (comicNumber: any) => {
+    fetch(`http://localhost:5000/xkcd/${comicNumber}`)
+      .then((response) => response.json())
+      .then((data) => setXkcdData(data))
+      .catch((error) => console.error("Error fetching XKCD data:", error));
+  };
+
+  const fetchLatestComic = () => {
+    fetch("http://localhost:5000/xkcd/latest")
+      .then((response) => response.json())
+      .then((data) => {
+        setXkcdData(data);
+        setLatestComicNumber(data.num);
+      })
+      .catch((error) => console.error("Error fetching XKCD data:", error));
+  };
+
+  const handlePrev = () => {
+    if (xkcdData && xkcdData?.num > 1) {
+      fetchComic(xkcdData?.num - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (xkcdData && xkcdData?.num < latestComicNumber) {
+      fetchComic(xkcdData?.num + 1);
+    }
+  };
+
+  const handleRandom = () => {
+    if (xkcdData && xkcdData?.num) {
+      const randomNumber =
+        Math.floor(Math.random() * (latestComicNumber - 0 + 1)) + 0;
+      fetchComic(randomNumber);
+    }
+  };
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              latestComic ? (
-                <ComicDisplay comic={latestComic} />
-              ) : (
-                <p>Loading...</p>
-              )
-            }
-          />
-          <Route path="/comic/:comicNumber" element={<ComicDisplay />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>XKCD Comic Viewer</h1>
+      {xkcdData && (
+        <div>
+          <h2>{xkcdData?.title}</h2>
+          <img src={xkcdData?.img} alt={xkcdData?.alt} />
+          <p>
+            <strong>Comic Number:</strong> {xkcdData?.num}
+          </p>
+          <p>
+            <strong>Date:</strong> {xkcdData?.day}
+          </p>
+          <p>
+            <em>{xkcdData?.alt}</em>
+          </p>
+          <button onClick={handlePrev}>Prev</button>
+          <button onClick={handleNext}>Next</button>
+          <button onClick={handleRandom}>Random</button>
+        </div>
+      )}
+    </div>
   );
 };
 
